9d7063716a697ca969b046978edcea58c2d66c65
Emre Koc <hi@emrekoc.io>
2023-02-25 15:18:23 +0000

    Add client add page

---
+++ src/pages/clients/add.tsx
@@ -0,0 +1,35 @@
+import { Form } from '@/components';
+import { gql, useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
+
+export default function ClientAdd() {
+
+    const router = useRouter();
+
+    const [mutate, mutation] = useMutation(gql`mutation($input: ClientFields) {
+        clientCreate (input: $input) {
+            address
+            currency
+            email
+            id
+            name
+        }
+    }`);
+
+    async function handleSubmit(values: IndexedObject) {
+        const result = await mutate({
+            variables: {
+                input: values,
+            },
+        });
+        router.push(`/clients/${result.data.clientCreate.id}`);
+    }
+
+    return <Form loading={mutation.data || mutation.loading} title="Add Client" onSubmit={handleSubmit}>
+        <Form.Field name="name" required />
+        <Form.Field name="currency" required />
+        <Form.Field name="email" />
+        <Form.Field name="address" type="textarea" />
+    </Form>;
+
+}
