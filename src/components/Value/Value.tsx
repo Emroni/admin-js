b93696cbc8b9241f0af892dcbb3202461ab33604
Emre Koc <hi@emrekoc.io>
2023-03-11 17:49:05 +0000

    Refactor Value

--- src/components/Value/Value.tsx
+++ src/components/Value/Value.tsx
@@ -6,37 +6,33 @@ import { useEffect, useState } from 'react';
 export default function Value({ currency, options, type, value }: ValueProps) {
 
     const [content, setContent] = useState<any>(undefined);
-    const [currencySymbol, setCurrencySymbol] = useState('');
     const [money, setMoney] = useState<string | null>(null);
+    const [moneySymbol, setMoneySymbol] = useState('');
 
     useEffect(() => {
-        // Get content
+        // Prepare content
         let newContent = value;
 
-        // Check array
+        // Check type
         if (Array.isArray(newContent)) {
             newContent = newContent.length;
-        }
 
-        // Check type
-        if (type === 'hours') {
-            newContent = hoursToTime(newContent);
-        } else if (type === 'money') {
-            // Get currency symbol
-            const newCurrencySymbol = CURRENCIES.find(c => c.name === currency)?.symbol || '';
-            setCurrencySymbol(newCurrencySymbol);
-
-            // Get money
-            const newMoney = (parseFloat(newContent) - parseInt(newContent)).toFixed(2).slice(1);
-            setMoney(newMoney);
-        }
-
-        // Check options
-        if (options) {
+        } else if (options) {
             const option = options.find(option => option.value === newContent || option.id === newContent || option.name === newContent);
-            if (option) {
-                newContent = option.label || option.name || newContent;
-            }
+            newContent = option?.label || option?.name || newContent;
+
+        } else if (type === 'hours') {
+            newContent = hoursToTime(newContent);
+
+        } else if (type === 'money') {
+            // Get money
+            const val = parseFloat(newContent);
+            const newMoney = (isNaN(val) ? 0 : val - parseInt(newContent)).toFixed(2).slice(1);
+            setMoney(newMoney);
+
+            // Get money symbol
+            const newMoneySymbol = CURRENCIES.find(c => c.name === currency)?.symbol || '';
+            setMoneySymbol(newMoneySymbol);
         }
 
         // Set content
@@ -51,7 +47,7 @@ export default function Value({ currency, options, type, value }: ValueProps) {
     if (money) {
         return <>
             <Typography color="grey.400" component="small" fontSize="smaller" marginRight={0.5}>
-                {currencySymbol}
+                {moneySymbol}
             </Typography>
             {parseInt(content)}
             <Typography color="grey.400" component="small" fontSize="smaller">
